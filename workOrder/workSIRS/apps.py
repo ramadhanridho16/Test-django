7fc37870dde7ab857ef3d1a1f532b1fa27537dfa
ridho <ramadhan.ridho.16@students.unnes.ac.id>
2024-03-25 13:03:58 +0700

    how to add logic in django

---
+++ workOrder/workSIRS/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WorksirsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'workSIRS'
---
+++ workOrder/workSIRS/models.py
@@ -0,0 +1,11 @@
+# models.py
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
